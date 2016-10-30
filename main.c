1a1e2af101464000c1f73814ae381be3e61b5319
Abhishek Jadhav <abhishekjadhav205@gmail.com>
2016-10-30 21:40:19 +0530

    Update main.c

--- main.c
+++ main.c
@@ -4,6 +4,20 @@
 *User will have to left double click to open or unflag a block
 *Left single click will be used to mark the flags
 */
+/*main.c
+	Copyright (C) 2016  ABHISHEK JADHAV
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include<stdio.h>
 #include<ncurses.h>
 #include<assert.h>
